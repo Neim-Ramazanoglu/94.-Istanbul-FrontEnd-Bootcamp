7d927d434cf181bf979805a88bc275019bc831a5
Neim-Ramazanoglu <mr.ramazanoglu@gmail.com>
2021-07-01 14:53:49 +0300

    final commit

---
+++ Hafta1-Odev1/index.js
@@ -0,0 +1,22 @@
+let name1 = "Neim"
+let surname1 = "Ramazanoğlu"
+
+document.getElementById('firstName'),document.getElementById('surname')
+console.log(firstName.innerHTML = name1, surname.innerHTML = surname1)
+
+
+document.getElementById('age')
+console.log(age.innerHTML = "27")
+
+let linkofLinkedin = document.getElementById('linkedin')
+linkofLinkedin.href = "https://www.linkedin.com/in/neimramazanoglu/"
+
+let linkofGitHub = document.getElementById('github')
+linkofGitHub.href = "https://github.com/Neim-Ramazanoglu"
+
+function myFunction(){
+
+    document.getElementById('buton').style.display = "none";
+    document.getElementById('nameAge').style.display = "block";
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+In this repo you will be able to check out all the homeworks and projects that I have done during the 94.Istanbul-FrontEnd-Bootcamp.
